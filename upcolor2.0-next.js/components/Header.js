f2525fc7fb8728eae74b05e621f3d8223dc0aa85
unknown <nishant.meher.9595@gmail.com>
2022-05-30 16:07:55 +0900

    Main Page

---
+++ upcolor2.0-next.js/components/Header.js
@@ -0,0 +1,15 @@
+
+
+function Header() {
+  return (
+    <header>
+        {/* { Left } */}
+        <div className="flex items-center space-x-2 w-full max-w-xs">
+        </div>
+
+        {/* { Right } */}
+    </header>
+  )
+}
+
+export default Header
