c112c2513ac0fbae452a7d89a294d5d4ff959e2d
unknown <nishant.meher.9595@gmail.com>
2022-06-02 16:39:59 +0900

    Header Changes

--- upcolor2.0-next.js/components/Header.js
+++ upcolor2.0-next.js/components/Header.js
@@ -10,24 +10,22 @@ import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import { Avatar } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const spring = {
-  type : "spring",
-  stiffness : 700,
-  damping : 30,
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
 };
-
-
 
 function Header() {
   const [mounted, setMounted] = useState(false);
-  const {setTheme, resolvedTheme, theme } = useTheme();
+  const { setTheme, resolvedTheme, theme } = useTheme();
 
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), []);
 
-  console.log("Current These is ", theme);
+  console.log("Current theme is", theme);
 
   return (
     <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
@@ -39,53 +37,53 @@ function Header() {
               <Image src="https://weblike-upcolor.ssl-lolipop.jp/UpColor/includes/images/Up.png" width={45} height={45} />
             ) : (
               <Image src="https://weblike-upcolor.ssl-lolipop.jp/UpColor/includes/images/Up.png" width={55} height={55} />
+              )}
+              </>
             )}
-          </>
-        )}
-
-        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
-          <SearchRoundedIcon />
-          <input
-            type="text"
-            placeholder="Search"
-            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
-          />
-        </div>
-      </div>
-
-        {/* { Right } */}
-        <div className="flex items-center space-x-6">
-        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
-        <HeaderLink Icon={GroupIcon} text="My Network" feed />
-        <HeaderLink Icon={BusinessCenterIcon} text="企業" feed hidden />
-        <HeaderLink Icon={ChatIcon} text="メッセージ" feed />
-        <HeaderLink Icon={NotificationsIcon} text="通知" feed />
-        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
-        <HeaderLink Icon={AppsOutlinedIcon} text="アプリケーション" feed hidden />
-
-         {/* Dark mode toggle */}
-         {mounted && (
-           <div
-           className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative
-           ${resolvedTheme === "dark" ? "justify-end" : "justify-start"}`}
-           onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-         >
-           <span className="absolute left-0">🌜</span>
-
-           {/* {motion.div} */}
-           <motion.div 
-           className="w-5 h-5 bg-white rounded-full z-40" 
-           layout transition={spring}
-           />
-           <span className="absolute right-0.5">🌞</span>
-         </div>
-
-         )}
-         
-          
-        </div>
-    </header>
-  );
-}
-
-export default Header
+    
+            <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+              <SearchRoundedIcon />
+              <input
+                type="text"
+                placeholder="Search"
+                className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+              />
+            </div>
+          </div>
+          {/* Right */}
+    
+          <div className="flex items-center space-x-6">
+            <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+            <HeaderLink Icon={GroupIcon} text="My Network" feed />
+            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+            <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+            <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+            <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+            <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+    
+            {/* Dark mode toggle */}
+            {mounted && (
+              <div
+                className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative ${
+                  resolvedTheme === "dark" ? "justify-end" : "justify-start"
+                }`}
+                onClick={() =>
+                  setTheme(resolvedTheme === "dark" ? "light" : "dark")
+                }
+              >
+                <span className="absolute left-0">🌜</span>
+                <motion.div
+                  className="w-5 h-5 bg-white rounded-full z-40"
+                  layout
+                  transition={spring}
+                />
+    
+                <span className="absolute right-0.5">🌞</span>
+              </div>
+            )}
+          </div>
+        </header>
+      );
+    }
+    
+    export default Header;
