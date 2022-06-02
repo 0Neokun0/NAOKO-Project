3be407b91d06099e588bda25f26de8ab7578ab39
unknown <nishant.meher.9595@gmail.com>
2022-06-02 16:00:33 +0900

    widget

--- upcolor2.0-next.js/next.config.js
+++ upcolor2.0-next.js/next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ['weblike-upcolor.ssl-lolipop.jp', 'github.com', 'www.theworkfamilyconnection.org', 'pbs.twimg.com'],
+    domains: ['weblike-upcolor.ssl-lolipop.jp', 'github.com', 'www.theworkfamilyconnection.org', 'pbs.twimg.com', 'i-seifu.jp'],
   },
 }
--- upcolor2.0-next.js/pages/api/posts/[id].js
+++ upcolor2.0-next.js/pages/api/posts/[id].js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../../util/mongobd";
+import { connectToDatabase } from "../../../util/mongodb";
 import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
